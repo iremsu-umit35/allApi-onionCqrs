4f0ffb081a4e8d1b5b540aca4a1a4e166df118f2
iremsu ümit <138526237+iremsu-umit35@users.noreply.github.com>
2025-08-27 01:28:29 +0300

    Temel Entityler oluşturuldu (brand category detail product class)

---
+++ core/allApı.Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using allApı.Domain.Commen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Bu sınıf bir markayı (örneğin ürün markası) temsil ediyor.
+namespace allApı.Domain.Entities
+{
+   public class Brand: EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ core/allApı.Domain/Entities/category.cs
@@ -0,0 +1,37 @@
+﻿using allApı.Domain.Commen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Category sınıfı, EntityBase sınıfından miras alıyor ve IEntityBase arayüzünü implemente ediyor.
+// Bu sınıf bir kategori bilgisini temsil ediyor (örneğin ürün kategorisi).
+// 3 tane property içeriyor: ParentId, Name ve Priority.
+// "required" anahtar sözcüğü ile bu property’lerin nesne oluşturulurken mutlaka verilmesi gerektiği belirtilmiş.
+// İki constructor var: 
+// 1) Parametresiz constructor → boş bir Category nesnesi oluşturmak için.
+// 2) Parametreli constructor → ParentId, Name ve Priority değerleri verilerek nesneyi initialize etmek için.
+
+namespace allApı.Domain.Entities
+{
+   public class category: EntityBase,IEntityBase 
+    {
+        public category()
+        {
+            
+        }
+        public category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
