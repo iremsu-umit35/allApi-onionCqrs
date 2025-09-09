8ca6252232b9710b46424f7bff932c8f4c23887a
iremsu ümit <138526237+iremsu-umit35@users.noreply.github.com>
2025-09-09 23:37:29 +0300

    repositiler yapıldı write ve read

---
+++ core/allApı.Application/Interfaces/Repositories/IReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using allApı.Domain.Commen;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allApı.Application.Interfaces.Repositories
+{
+    //Bu IReadRepository<T> arayüzü (interface), veritabanından
+    //okuma (read) işlemlerini soyutlamak için kullanılıyor.
+    //Yani veri çekme işleri için ortak bir nokta sağlıyor.
+    public interface  IReadRepository<T> where T : class, IEntityBase,new()
+    {
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            bool enableTracking =false);
+
+        Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null,
+           Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+           Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+           bool enableTracking = false, int currentPage=1, int pageSize=3);//mevcut sayfadaki ilk 3 veriyi alıyor
+
+        Task<IList<T>> GetAsync(Expression<Func<T, bool>> predicate ,
+           Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+           bool enableTracking = false);
+
+
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking = false);
+
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    }
+}
+//GetAllAsync(x=>x.include (x=>x.Bramd).theninclude)
---
+++ core/allApı.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,19 @@
+﻿using allApı.Domain.Commen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allApı.Application.Interfaces.Repositories
+{
+   public interface IWriteRepository <T> where T : class, IEntityBase, new()
+    {
+        Task AddAsync(T entity);//Yeni bir entity ekler (ör. yeni kullanıcı eklemek). etkilenen kayıt sayısını veya yeni kaydın Id’sini döner.
+        Task AddRangeAsync(T entities);//Aynı anda birden fazla entity eklemek için kullanılır.
+        Task<T> UpdateAsync(T entity);//Var olan bir entity’yi günceller (ör. kullanıcı adını değiştirmek).
+        Task HardDeleteAsync(T entity);//Hard delete: kaydı veritabanından tamamen siler.id ile siliniyor
+
+    }
+}
+ 
